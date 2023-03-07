bc72f91284248b94bc6e9e07592117b1bfc0f347
Matthew Luchini <luch@Matthews-MBP.lan>
2023-03-06 19:14:05 -0500

    5/6 ALL CHALLENGES DONE

--- ChallengesWithTestsMark8/ChallengesSet05.cs
+++ ChallengesWithTestsMark8/ChallengesSet05.cs
@@ -7,37 +7,125 @@ namespace ChallengesWithTestsMark8
     {
         public int GetNextNumberDivisibleByN(int startNumber, int n)
         {
-            throw new NotImplementedException();
+            int output = ((startNumber / n) + 1) * n;
+            return output;
         }
 
         public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
         {
-            throw new NotImplementedException();
+
+            for (var x = 0; x < businesses.Length; x++)
+            {
+                if (businesses[x].TotalRevenue == 0)
+                {
+                    businesses[x].Name = "CLOSED";
+                }
+
+            }
         }
 
         public bool IsAscendingOrder(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return false;
+            }
+            if (numbers.Length == 0)
+            {
+                return false;
+            }
+            for (var x = 1; x < numbers.Length; x++)
+            {
+                if (numbers[x] < numbers[x - 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
-        public int SumElementsThatFollowAnEven(int[] numbers)
+    
+
+    public int SumElementsThatFollowAnEven(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+
+            var sum = 0;
+
+            for (var x = 1; x < numbers.Length; x++)
+            {
+                if (numbers[x - 1] % 2 == 0)
+                {
+                    sum += numbers[x];
+                }
+            }
+
+            return sum;
         }
 
         public string TurnWordsIntoSentence(string[] words)
         {
-            throw new NotImplementedException();
+            if (words == null || words.Length == 0)
+            {
+                return "";
+            }
+
+            string sentence = "";
+
+            foreach (string word in words)
+            {
+                if (word.Trim().Length > 0)
+                {
+                    sentence += word.Trim() + " ";
+                }
+            }
+
+            if (sentence.Length == 0)
+            {
+                return "";
+            }
+
+            sentence = sentence.Substring(0, sentence.Length - 1);
+            sentence += ".";
+            return sentence;
         }
 
         public double[] GetEveryFourthElement(List<double> elements)
         {
-            throw new NotImplementedException();
+            List<double> everyFourth = new List<double>();
+
+            if (elements == null)
+            {
+                return new double[] { };
+            }
+
+            for (int i = 3; i < elements.Count; i += 4)
+            {
+                everyFourth.Add(elements[i]);
+            }
+
+            return everyFourth.ToArray();
         }
 
         public bool TwoDifferentElementsInArrayCanSumToTargetNumber(int[] nums, int targetNumber)
         {
-            throw new NotImplementedException();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int k = i + 1; k < nums.Length; k++)
+                {
+                    if (nums[i] + nums[k] == targetNumber)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         }
     }
 }
